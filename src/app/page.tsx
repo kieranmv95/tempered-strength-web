c9ad2807c7da17fa7db48cfe7aaef7a6f435dcbb
kieranvenison <venison@hey.com>
2024-08-15 08:29:24 +0100

    refactor: uppercase and prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
     <main>
-      <div>
-        <h1>Tempered Strength</h1>
-        <p>Coming Soon</p>
+      <div className="w-full h-screen bg-zinc-800 text-white grid items-center">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">TEMPERED STRENGTH</h1>
+          <p>Coming Soon</p>
+        </div>
       </div>
     </main>
   );
